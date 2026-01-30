9d3873dc63d54daf1067f876d16a2bf8efb16a5b
Sacky <89039310+sackuspl@users.noreply.github.com>
2026-01-30 12:26:37 +0100

    asdsad

---
+++ latest-video.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const YT_LINK = "https://www.youtube.com/watch?v=8q-Hul7fh_U";
+  document.getElementById("fake-yt-embed").href = YT_LINK;
+});

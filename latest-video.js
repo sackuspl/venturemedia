066b643e57039f612471f5e77c72f9eb3cf79367
Sacky <89039310+sackuspl@users.noreply.github.com>
2026-02-24 15:53:14 +0100

    saas

--- latest-video.js
+++ latest-video.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const YT_LINK = "https://www.youtube.com/watch?v=8q-Hul7fh_U";
+  const YT_LINK = "https://www.youtube.com/watch?v=McSWqrVmHMk&t";
   document.getElementById("fake-yt-embed").href = YT_LINK;
 });
